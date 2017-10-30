bc5e7b52dc920682cdce6757e59956dce505b32f
Miguel A. Nuñez <miguel.nunez.casado@gmail.com>
2017-10-30 15:45:43 +0100

    Initial commit

---
+++ app/src/main/java/masterlistas/example/org/masterlistas/ListasActivity.java
@@ -0,0 +1,24 @@
+package masterlistas.example.org.masterlistas;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ListasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listas);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
