132d4d9983d5e8c014f69c846978055c7be4991d
Miguel A. Nuñez <miguel.nunez.casado@gmail.com>
2017-10-31 01:23:27 +0100

    Añadidas imágenes Avance en Tema

---
+++ app/src/main/java/masterlistas/example/org/masterlistas/Lista.java
@@ -0,0 +1,41 @@
+package masterlistas.example.org.masterlistas;
+
+/**
+ * Created by seven on 30/10/2017.
+ */
+
+public class Lista {
+    private int imagen;
+    private String nombre;
+    private int elementos;
+
+    public Lista(int imagen, String nombre, int elementos) {
+        this.imagen = imagen;
+        this.nombre = nombre;
+        this.elementos = elementos;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getElementos() {
+        return elementos;
+    }
+
+    public void setElementos(int elementos) {
+        this.elementos = elementos;
+    }
+}
