0e34e52dc2942206e3ca71e6840d298261a10902
Miguel A. Nuñez <miguel.nunez.casado@gmail.com>
2017-11-27 15:00:43 +0100

    Tarea finalizada

--- app/src/main/java/masterlistas/example/org/masterlistas/ListasActivity.java
+++ app/src/main/java/masterlistas/example/org/masterlistas/ListasActivity.java
@@ -1,11 +1,16 @@
 package masterlistas.example.org.masterlistas;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,6 +20,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
+import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,11 +35,16 @@ import java.util.List;
  * Created by Miguel Á. Núñez on 30/10/2017.
  */
 
-public class ListasActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ListasActivity extends AppCompatActivity {
 
     private RecyclerView recycler;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager lManager;
+    private FlowingDrawer mDrawer;
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseRemoteConfig remoteConfig;
+    private static final int CACHE_TIME_SECONDS = 30; // 30 segundos
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +59,52 @@ public class ListasActivity extends AppCompatActivity implements NavigationView.
             }
         });
 
+        Transition lista_enter = TransitionInflater.from(this).inflateTransition(R.transition.transition_lista_enter);
+        getWindow().setEnterTransition(lista_enter);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
         // Toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
+        Toolbar toolbar = findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         // Navigation Drawer
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        NavigationView navigationView = findViewById(R.id.vNavigation);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        mDrawer = findViewById(R.id.drawerlayout);
+        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
+
+        mDrawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
+            @Override
+            public void onDrawerStateChange(int oldState, int newState) {
+                if (newState == 2 || newState == 8) {
+                    mFirebaseAnalytics.logEvent("Navigation_drawer_open", new Bundle());
+                }
+            }
+
+            @Override
+            public void onDrawerSlide(float openRatio, int offsetPixels) {
+
+            }
+        });
+
+        toolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mDrawer.toggleMenu();
+            }
+        });
 
         //Inicializar los elementos
         List items = new ArrayList();
@@ -70,34 +123,53 @@ public class ListasActivity extends AppCompatActivity implements NavigationView.
         recycler.addOnItemTouchListener(new RecyclerItemClickListener(ListasActivity.this, new RecyclerItemClickListener.OnItemClickListener() {
             @Override
             public void onItemClick(View v, int position) {
-                Toast.makeText(ListasActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ListasActivity.this, DetalleListaActivity.class);
+                intent.putExtra("numeroLista", position);
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(ListasActivity.this, new Pair<View, String>(v.findViewById(R.id.imagen), getString(R.string.transition_name_img)));
+                ActivityCompat.startActivity(ListasActivity.this, intent, options.toBundle());
             }
         }));
-    }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.nav_1) {
-            // …
-        } else if (id == R.id.nav_2) {
-            // …
-        } else if (id == R.id.nav_3) {
-            // …
-        } // …
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        remoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings config = new FirebaseRemoteConfigSettings.Builder().setDeveloperModeEnabled(BuildConfig.DEBUG).build();
+        remoteConfig.setConfigSettings(config);
+        remoteConfig.setDefaults(R.xml.remote_config);
+        remoteConfig.fetch(CACHE_TIME_SECONDS).addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(ListasActivity.this, "Fetch OK", Toast.LENGTH_SHORT).show();
+                    remoteConfig.activateFetched();
+                } else {
+                    Toast.makeText(ListasActivity.this, "Fetch ha fallado", Toast.LENGTH_SHORT).show();
+                }
+                String openDrawer = remoteConfig.getString("navigation_drawer_abierto");
+                if (openDrawer.equals("abrir_si")) {
+                    mFirebaseAnalytics.setUserProperty( "exp_nav_drawer", "abrir_si" );
+                    abrePrimeraVez();
+                } else {
+                    mFirebaseAnalytics.setUserProperty( "exp_nav_drawer", "abrir_no" );
+                }
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (mDrawer.isMenuVisible()) {
+            mDrawer.closeMenu();
         } else {
             super.onBackPressed();
+        }
+    }
+
+    public void abrePrimeraVez() {
+        SharedPreferences sp = getSharedPreferences("mispreferencias", 0);
+        boolean primerAcceso = sp.getBoolean("abrePrimeraVez", true);
+        if (primerAcceso) {
+            mDrawer.openMenu();
+            SharedPreferences.Editor e = sp.edit();
+            e.putBoolean("abrePrimeraVez", false).commit();
         }
     }
 }
